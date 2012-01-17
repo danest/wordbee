b40a0bc109d9657d2e6d9a336b427e2f167f70f0
Kevin Urrutia <urrutia.kevin@gmail.com>
2012-01-16 21:05:38 -0500

    making sinatra word base game public for the first time

---
+++ app.rb
@@ -0,0 +1,114 @@
+require 'sinatra'
+require 'datamapper'
+require 'json'
+require 'nokogiri'
+require 'open-uri'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/wordbee.db")
+
+class Word
+  include DataMapper::Resource
+  property :id, Serial
+  property :speech, Text, :required => true
+  property :word, Text, :required => true
+  property :definition, Text, :required => true
+  property :created_at, DateTime  
+  property :updated_at, DateTime  
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  erb :index
+end
+
+get '/word/:id' do
+    content_type :json
+    @word = Word.get params[:id]
+    {
+      :word => @word.word,
+      :speech => @word.speech,
+      :definition => @word.definition
+    }.to_json
+end
+
+get '/lookup/:w' do
+  content_type :json
+  @word = Word.all(:word.like => params[:w].lower)
+  { :word => @word }.to_json  
+end
+
+get '/makeword' do
+    #TODO move this into its own class
+    url = "http://www.thefreedictionary.com/dictionary.htm"
+    doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+    #map all the words to extract the def
+    #40 random words at once
+    doc.css(".box1 td a").map{ |word|
+
+      puts word.text.capitalize
+      #sleep the script or else the server detects and sends a 403 forbidden response
+      sleep 5
+      url ="http://www.thefreedictionary.com/" + word
+      doc = Nokogiri::HTML(open(url))
+      #puts doc.at_css(".ds-single").text
+      if(doc.at_css(".sds-list") && doc.at_css(".ds-list") == nil )
+        definition =  doc.at_css(".sds-list").text
+      elsif(doc.at_css(".ds-list") && doc.at_css(".ds-single") == nil)
+        definition = doc.at_css(".ds-list").text
+      elsif(doc.at_css(".sds-list") && doc.at_css(".ds-list"))
+        definition = doc.at_css(".ds-list").text
+      else
+        definition = doc.at_css(".ds-single").text
+      end
+
+      if definition.include? "1. a.  "
+        #puts definition
+        definition = definition[7, definition.length]
+      elsif definition.include? "1. "
+       # puts definition
+        definition = definition[3, definition.length]
+      end
+
+      puts definition.strip
+
+
+      if(doc.at_css("div i").text.strip == "n.")
+        speech =  "Noun"
+      elsif(doc.at_css("div i").text.strip == "adj.")
+         speech =  "Adjective"
+      elsif(doc.at_css("div i").text.strip == "v.")
+         speech =  "Verb"
+      elsif(doc.at_css("div i").text.strip == "pron.")
+         speech =   "Pronoun"
+      elsif(doc.at_css("div i").text.strip == "adv.")
+         speech =   "Adverb"
+      elsif(doc.at_css("div i").text.strip == "prep.")
+         speech =   "Preposition"
+      elsif(doc.at_css("div i").text.strip == "conj.")
+          speech =   "Conjunction"
+      elsif(doc.at_css("div i").text.strip == "interj.")
+          speech =   "Interjection"
+      elsif(doc.at_css("div i").text.strip == "tr.v.")
+         speech =   "Transitive Verb"
+      elsif(doc.at_css("div i").text.strip == "abbr.")
+         speech =   "Abbreviation"
+      elsif(doc.at_css("div i").text.strip == "intr.v.")
+         speech =   "Intransitive Verb"
+      else
+         speech =   nil
+      end
+
+      if speech
+        w = Word.new
+        w.speech = speech
+        w.word = word.text.capitalize
+        w.definition = definition
+        w.created_at = Time.now
+        w.updated_at = Time.now
+        w.save
+        # db.execute( "INSERT OR REPLACE into words values (?, ?, ?, ? )",nil, speech, word.text.capitalize, definition)
+      end
+ }
+  redirect '/'
+end
---
+++ public/application.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+  // do stuff here now
+  mode = "easy"
+  app = {
+    makeWord: function(){
+      $.ajax({
+        url: '/word/'+app.addOne(),
+        success: function(data, xhr){
+          console.log(data);
+          app.addWord(data);
+        //alert(data.name);
+        //console.log(xhr);
+        },
+        error: function(data){
+        console.log(data.status);
+        }
+      });
+    },
+
+    addWord: function(data){
+      $('#speech').html(data.speech);
+      $('#definition').html(data.definition);
+      $('#word').html(data.word).data('currentword',data.word);
+	  $('input#word').val(app.word_mode(mode,data.word));
+    },
+
+	chooseMode: function(){
+	  $("#levels span").click(function(){
+		console.log(this);
+		$("#levels span.active").removeClass('active');
+		var text = $(this).attr('id');
+		if(text === 'hard')
+			mode = 'hard';
+		else if(text == 'medium')
+			mode = 'medium'
+		else
+			mode = 'easy'
+		$(this).addClass('active');
+		app.makeWord();
+	});
+	},
+
+	word_mode: function(current_mode,data){
+		if(current_mode == "easy"){
+			return data.substring(0,3);
+		}
+		if(current_mode == "medium"){
+			return data.substring(0,2);
+		}
+		if(current_mode == "hard"){
+			return data.substring(0,1);
+		}
+	},
+	
+    addOne: function(){
+      var current = 1;
+      //return an object
+      return (function(){
+          current = Math.floor(Math.random()*40);
+          return current;
+      })();
+   }
+}
+
+	//check word and add points to score
+  $("#checkword").submit(function(e){
+	var inputword = $('input#word').val().toLowerCase();
+	if (inputword === ''){
+		e.preventDefault();
+	    return;
+	}
+    console.log($('input#word').val());
+    console.log($('#word').data('currentword'))
+    if(inputword === $('#word').data('currentword').toLowerCase()){
+      console.log('+10');
+	  var points = parseInt($("#points").text());
+	  points +=10;
+	  $("#points").text(points.toString());
+	  $("#wrong").text("Correct answer, you recieved 10 points");
+    } else {
+	  $("#wrong").text("Sorry wrong answer");
+    }
+	$("#wrong").effect("highlight", {}, 1500);
+    app.makeWord();
+    $('input#word').val('');
+    e.preventDefault();
+  });
+
+
+
+
+var app = app;
+  app.chooseMode();
+  //var num = app.addOne();
+
+  //generate one word at page load
+  //FIX when current = 0?
+	$('#choose').click(function(){
+	   $('#levels').slideDown();
+	});
+	
+	$('#close').click(function(){
+		$('#levels').slideUp();
+	});
+
+	$('#start').click(function(){
+	  app.makeWord()
+	});
+	
+});
+
+
